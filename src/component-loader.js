bc298c0e0aac0c5a46532f1f4e0324cd19a9bbd6
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:19:41 -0400

    update to koco2

--- src/component-loader.js
+++ src/component-loader.js
@@ -1,81 +1,84 @@
-define(['knockout', 'lodash'],
-    function(ko, _) {
-        'use strict';
-        return {
+import ko from 'knockout';
+import _ from 'lodash';
 
-            loadViewModel: function(name, componentConfig, callback) {
-                //pour bypass
-                //callback(null);
+export default {
 
-                if (componentConfig.htmlOnly === true) {
-                    callback(null);
-                }
+    loadViewModel: function(name, componentConfig, callback) {
+        //pour bypass
+        //callback(null);
 
-                if (componentConfig.type === 'page') {
-                    ko.components.defaultLoader.loadViewModel(name, { createViewModel: function(viewModel, componentInfo) {
-                        if (viewModel && viewModel.init) {
-                            viewModel.init(componentInfo);
-                        }
-
-                        return viewModel;
-                    } } , callback);
-                } else {
-
-
-                    ko.components.defaultLoader.loadViewModel(name, {createViewModel: function(params, componentInfo) {
-                        if (_.isFunction(componentConfig)) {
-                            return new componentConfig(params, componentInfo);
-                        }
-
-                        return componentConfig;
-                    }}, callback);
-                }
-            },
-            loadComponent: function(name, componentConfig, callback) {
-                //pour bypass
-                //callback(null);
-
-                var kocoComponentConfig = createComponentConfigWithKocoConventions(name, componentConfig);
-                ko.components.defaultLoader.loadComponent(name, kocoComponentConfig, callback);
-            }
-        };
-
-        function createComponentConfigWithKocoConventions(name, componentConfig) {
-            var basePath = componentConfig.basePath || 'components/' + name;
-
-            if (!componentConfig.type) {
-                componentConfig.type = 'component';
-            }
-
-            if (componentConfig.isBower) {
-                basePath = 'bower_components/koco-' + name + '/src';
-            }
-
-            var requirePath = basePath + '/' + name;
-
-            var templateRequirePath;
-
-            if (componentConfig.template) {
-                templateRequirePath = 'text!' + componentConfig.template;
-            } else {
-                templateRequirePath = 'text!' + requirePath + '.html';
-            }
-
-            componentConfig.template = {
-                require: templateRequirePath
-            };
-
-            if (componentConfig.htmlOnly !== true) {
-                if (componentConfig.type === 'page') {
-                    componentConfig.viewModel = componentConfig;
-                } else {
-                    componentConfig.viewModel = {
-                        require: requirePath + '-ui'
-                    };
-                }
-
-            }
-
-            return componentConfig;
+        if (componentConfig.htmlOnly === true) {
+            callback(null);
         }
-    });
+
+        if (componentConfig.type === 'page') {
+            ko.components.defaultLoader.loadViewModel(name, {
+                createViewModel: function(viewModel, componentInfo) {
+                    if (viewModel && viewModel.init) {
+                        viewModel.init(componentInfo);
+                    }
+
+                    return viewModel;
+                }
+            }, callback);
+        } else {
+
+
+            ko.components.defaultLoader.loadViewModel(name, {
+                createViewModel: function(params, componentInfo) {
+                    if (_.isFunction(componentConfig)) {
+                        return new componentConfig(params, componentInfo);
+                    }
+
+                    return componentConfig;
+                }
+            }, callback);
+        }
+    },
+    loadComponent: function(name, componentConfig, callback) {
+        //pour bypass
+        //callback(null);
+
+        var kocoComponentConfig = createComponentConfigWithKocoConventions(name, componentConfig);
+        ko.components.defaultLoader.loadComponent(name, kocoComponentConfig, callback);
+    }
+};
+
+function createComponentConfigWithKocoConventions(name, componentConfig) {
+    var basePath = componentConfig.basePath || 'components/' + name;
+
+    if (!componentConfig.type) {
+        componentConfig.type = 'component';
+    }
+
+    if (componentConfig.isBower) {
+        basePath = 'bower_components/koco-' + name + '/src';
+    }
+
+    var requirePath = basePath + '/' + name;
+
+    var templateRequirePath;
+
+    if (componentConfig.template) {
+        templateRequirePath = 'text!' + componentConfig.template;
+    } else {
+        templateRequirePath = 'text!' + requirePath + '.html';
+    }
+
+    componentConfig.template = {
+        require: templateRequirePath
+    };
+
+    if (componentConfig.htmlOnly !== true) {
+        if (componentConfig.type === 'page') {
+            componentConfig.viewModel = componentConfig;
+        } else {
+            componentConfig.viewModel = {
+                require: requirePath + '-ui'
+            };
+        }
+
+    }
+
+    return componentConfig;
+}
